3c731464bad7d2c065c2d5a42b98bd27fea64120
Trixie <trixielavadiasoriano@gmail.com>
2023-07-06 09:40:44 +0800

    Updated changes

--- index.php
+++ index.php
@@ -9,9 +9,26 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>HOME</title>
+    <link rel="stylesheet" href="css/home.css">
+
 </head>
 <body>
-    
+    <div class="front">
+        <div class="picture">
+            <img src="css/img/that.png">
+        </div>
+        <div class="greetings">
+            <div class="name">
+                <h5>I AM</h5>
+                <h1> TRIXIE SORIANO </h1>
+                <h4>FOURTH YEAR - INFORMATION TECHNOLOGY STUDENT </h4>
+            </div>
+            <div class="btn">
+                <button>HIRE ME</button>
+                <button>RESUME</button>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
 
--- navbar.php
+++ navbar.php
@@ -10,6 +10,11 @@
 </head>
 <body>
     <nav>
+    <div class="burger">
+        <div class="line1"></div>
+        <div class="line2"></div>
+        <div class="line3"></div>
+    </div>
         <div class="logo">
             <a href="index.php">IXIECODE</a>
         </div>
@@ -19,6 +24,33 @@
             <li><a href="projects.php" <?php if ($activePage == 'projects') echo 'class="active"'; ?>>PROJECTS</a></li>
             <li><a href="contact.php" <?php if ($activePage == 'contact') echo 'class="active"'; ?>>CONTACT</a></li>
         </ul>
-    </nav>    
+    </nav>  
+    <script>
+        const navSlide = () => {
+        const burger = document.querySelector('.burger');
+        const nav = document.querySelector('.menu');
+        const navLinks = document.querySelectorAll('.menu li');
+
+        burger.addEventListener('click', () => {
+            // Toggle nav-active class on the nav element
+            nav.classList.toggle('nav-active');
+
+            // Toggle the visibility and animation of navLinks using the forEach loop
+            navLinks.forEach((link, index) => {
+                if (link.style.animation) {
+                    link.style.animation = '';
+                } else {
+                    link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+                }
+            });
+
+            // Toggle the toggle class on the burger element
+            burger.classList.toggle('toggle');
+        });
+    };
+
+    navSlide();
+
+    </script>  
 </body>
 </html>
