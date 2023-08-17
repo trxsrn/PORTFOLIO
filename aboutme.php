4a802531384b24903a1ed428a38183631784e4ed
Trixie <trixielavadiasoriano@gmail.com>
2023-08-17 09:33:38 +0800

    Update changes

---
+++ aboutme.php
@@ -0,0 +1,71 @@
+<?php
+    $activePage = 'about';
+    include 'navbar.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ABOUT ME</title>
+    <link rel="stylesheet" href="css/about.css">
+</head>
+<body>
+<div class="about">
+    <div class="about-details">
+        <div class="details">
+            <h3>ABOUT ME</h3>
+        </div>
+    </div>
+
+    
+    <p> Motivated third-year college student from <b>Rizal Technological University </b>actively seeking opportunities for self-development and acquiring new skills. Committed to continuous learning and open to diverse experiences. Eager to collaborate with like-minded individuals and build meaningful connections. Proactive in developing practical skills such as problem-solving, communication, leadership, and adaptability.</p>
+    <div class="header">
+      <h1>EDUCATIONAL BACKGROUND</h1>
+      <div class="line">&nbsp</div>
+    </div>
+    <div class="timeline">
+        <div class="img left">
+            <img src="css/img/rtu.jpeg">
+        </div>
+        <div class="container right">
+        <div class="content">
+          <h4>Tertiary</h4>
+          <h2><b>Rizal Technolological University</b></h2>
+          <p>Boni Ave, Mandaluyong, 1550 Metro Manila</p>
+           <p>2020 - Present</p>
+           <a href="#" class="lrn-school">LEARN MORE </a>
+        </div>
+      </div>
+      
+        <div class="img right">
+            <img src="css/img/rtu.jpeg">
+        </div>
+        <div class="container left">
+        <div class="content">
+          <h4>Secondary</h4>
+          <h2><b>Rizal High School</b></h2>
+          <p>Boni Ave, Mandaluyong, 1550 Metro Manila</p>
+           <p>2020 - Present</p>
+           <a href="#" class="lrn-school">LEARN MORE </a>
+        </div>
+      </div>
+      <div class="img left">
+        <img src="css/img/rtu.jpeg">
+      </div>
+            <div class="container right">
+        <div class="content">
+          <h4>Primary</h4>
+          <h2><b>Pasig Elementary School </b></h2>
+          <p>Boni Ave, Mandaluyong, 1550 Metro Manila</p>
+           <p>2020 - Present</p>
+           <a href="#" class="lrn-school">LEARN MORE </a>
+        </div>
+      </div>
+    </div>
+</div>
+</body>
+</html>
+
+<?php include 'footer.php' ?>
